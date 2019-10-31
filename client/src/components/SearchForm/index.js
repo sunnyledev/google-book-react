4f8b377c5285b1f2bd10ddec428ad1671d595e20
Sunny Le <sunny.le@capco.com>
2019-10-31 12:48:14 -0400

    Done styling for search result and edited styling for search form

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -4,10 +4,9 @@ import './style.css';
 const SearchForm = (props) => {
     return(
         <div className="search-form-outer">
-            <p className="search-label">Search for a Book:</p>
             <div className="form-group">
-                <input className="form-control" {...props} />
-                <button {...props} className="search-btn">
+                <input className="book-name-input shared-attrs" {...props} placeholder="Better read than dead... so let me find a book for you"/>
+                <button {...props} className="search-btn shared-attrs">
                     Search
                 </button>
             </div>
